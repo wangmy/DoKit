fa8fad545400c3073347125b7763dfa95afe59af
wangmy <wmy19890301@163.com>
2025-05-16 13:44:43 +0800

    update: delete 功能页面拉取接口逻辑

--- iOS/DoraemonKit/Src/Core/Entry/Home/Settings/DoraemonSettingViewController.m
+++ iOS/DoraemonKit/Src/Core/Entry/Home/Settings/DoraemonSettingViewController.m
@@ -40,24 +40,39 @@
 }
 
 - (void)loadData {
-    
     WEAKSELF(weakSelf)
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURL *url = [NSURL URLWithString:@"https://star.xiaojukeji.com/config/get.node?city=-1&areaid=&name=group"];
-    NSURLSessionTask *task = [session dataTaskWithURL:url
-                                    completionHandler:^(NSData *data, NSURLResponse *response, NSError* error) {
-        if (error == nil) {
-//            NSLog(@"%@", [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil]);
-            NSDictionary *dataDic = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-            weakSelf.dataArr = [[dataDic objectForKey:@"data"] objectForKey:@"group"];
-            
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [weakSelf.tableView reloadData];
-            });
-        }
-    }];
-
-    [task resume];
+    NSString *jsonString = @"{\"success\":true,\"data\":{\"group\":[{\"group\":\"本地功能\",\"list\":[{\"type\":\"native\",\"name\":\"功能管理\",\"desc\":\"介绍:可以针对dokit的内置工具列表进行自定义排序\",\"link\":\"dokit:\/\/native\/function_manager\"}]},{\"group\":\"官方消息\",\"list\":[{\"type\":\"web\",\"name\":\"dokit官网\",\"desc\":\"介绍:dokit.cn你值得拥有的APP研发助手一站式解决方案。\",\"link\":\"https:\/\/www.dokit.cn\"},{\"type\":\"web\",\"name\":\"dokit操作手册\",\"desc\":\"介绍:dokit官方操作手册\",\"link\":\"http:\/\/xingyun.xiaojukeji.com\/docs\/dokit\/#\/intro\"}]}]},\"code\":200}";
+    
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error = nil;
+    NSDictionary *dataDic = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
+    
+    if (error) {
+        NSLog(@"Error converting JSON string to dictionary: %@", error.localizedDescription);
+    } else {
+        NSLog(@"%@", dataDic);
+        weakSelf.dataArr = [[dataDic objectForKey:@"data"] objectForKey:@"group"];
+    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [weakSelf.tableView reloadData];
+    });
+    
+//    NSURLSession *session = [NSURLSession sharedSession];
+//    NSURL *url = [NSURL URLWithString:@"https://star.xiaojukeji.com/config/get.node?city=-1&areaid=&name=group"];
+//    NSURLSessionTask *task = [session dataTaskWithURL:url
+//                                    completionHandler:^(NSData *data, NSURLResponse *response, NSError* error) {
+//        if (error == nil) {
+////            NSLog(@"%@", [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil]);
+//            NSDictionary *dataDic = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+//            weakSelf.dataArr = [[dataDic objectForKey:@"data"] objectForKey:@"group"];
+//            
+//            dispatch_async(dispatch_get_main_queue(), ^{
+//                [weakSelf.tableView reloadData];
+//            });
+//        }
+//    }];
+//
+//    [task resume];
 }
 
 #pragma mark -- DoraemonCellButtonDelegate
